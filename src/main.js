6f58523c2d05ca394029d326c3f03b254927de21
7y7y <873492647@qq.com>
2020-03-15 11:07:53 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 完整导入vant  后期优化需要按需加载
+// 引用Vant组件
+import Vant from 'vant'
+// 引入样式
+import 'vant/lib/index.css'
+// 注册Vant组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
