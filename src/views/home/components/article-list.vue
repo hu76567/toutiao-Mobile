8dcfac9b9461605bc38faa41c8c6e5c52235ce52
7y7y <h85580@gmail.com>
2020-03-19 09:43:03 +0800

    模拟数据上拉加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,10 +1,13 @@
 <template>
   <!-- 文章列表组件 -->
   <!-- 放置这个div的目的是形成滚动条,用于做阅读记忆 -->
-  <div class="scrollwrapper">
-      <!-- 当组件滚动到底部时，会触发load事件 loading => true -->
-        <van-list @load="onLoad" :finished="finished" v-model="loading"></van-list>
-        <!-- 初始化后就会检测长度 超过限定就会执行 load 把loading设成true -->
+  <div class="scroll-wrapper">
+      <!-- 当组件滚动到底部时， 检测长度 会触发load事件 loading => true -->
+        <van-list finished-text="亲,没有数据了哦" @load="onLoad" :finished="finished" v-model="loading">
+          <van-cell-group >
+            <van-cell v-for="item in articles" :key="item" title="美股熔断" :value="'天台排队'+item"></van-cell>
+          </van-cell-group>
+        </van-list>
   </div>
 </template>
 
@@ -14,14 +17,24 @@ export default {
   data () {
     return {
       loading: false, // 是否开启上拉加载,默认为false
-      finished: false // 是否完成所有数据加载
+      finished: false, // 是否完成所有数据加载
+      articles: [] // 文章列表
     }
   },
   methods: {
     onLoad () {
-      setTimeout(() => {
+      // setTimeout(() => {
+      //   this.finished = true
+      // }, 1500)
+      if (this.articles.length > 50) {
         this.finished = true
-      }, 1500)
+      } else {
+        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        // 把数据加载到数组的队尾
+        this.articles.push(...arr)
+        // 添加完数据关闭loading
+        this.loading = false
+      }
     }
   }
 }
