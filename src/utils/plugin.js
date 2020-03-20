a4a7d98bd635d44d2f16975f041807cf77cb4752
7y7y <h85580@gmail.com>
2020-03-20 09:56:40 +0800

    时间格式处理

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,5 +1,16 @@
 // 提供小函数以及vue中常用的过滤器及函数
 
+// 时间处理初始化
+
+// 引入dayjs
+import dayjs from 'dayjs'
+// 引入相对时间插件
+import relativeTime from 'dayjs/plugin/relativeTime'
+// 引入语言包
+import 'dayjs/locale/zh-cn'
+// dayjs扩展相对时间的方法
+dayjs.extend(relativeTime)
+
 export default {
   // 该方法会在vue.use()时调用
   install (Vue) {
@@ -14,15 +25,26 @@ export default {
 
     // 定义了一个原型属性,所有组件都有这个属性
     Vue.prototype.$sleep = sleep
+
+    // 全局过滤器  处理时间
+    Vue.filter('relTime', relTime)
   }
 }
 
-// 休眠函数
-// time=500默认值 如果传入了值会更新此值
+// 休眠函数  time=500默认值 如果传入了值会更新此值
 function sleep (time = 500) {
   // 返回一个promise
   return new Promise(function (resolve, reject) {
     // 成功执行  但是要延时
     setTimeout(() => { resolve() }, time)
   })
+}
+
+/** 相对时间的处理 */
+// date 是传入的日期
+function relTime (date) {
+  // dayjs是当前时间
+  // from中是传入时间
+  // locale 语言转换
+  return dayjs().locale('zh-cn').from(date)
 }
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -32,7 +32,8 @@
               <div class="info_box">
                 <span>{{item.aut_name}}</span>
                 <span>{{item.comm_count}}评论</span>
-                <span>{{item.pubdate}}分钟前</span>
+                <!-- 使用过滤器 -->
+                <span>{{item.pubdate | relTime}}</span>
                 <span class="close">
                   <van-icon name="cross"></van-icon>
                 </span>
