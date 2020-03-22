689a72c1b6779d035b94f97a2c84b68eb88e25ce
7y7y <h85580@gmail.com>
2020-03-22 09:34:46 +0800

    编辑组件弹出

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,14 +7,15 @@
            <!-- 列表单元格组件 -->
           <!-- 监听子组件中的showAction -->
            <ArticleList @showAction="openAction" :channel_id="item.id"></ArticleList>
-          </van-tab>
+        </van-tab>
      </van-tabs>
      <!-- 在tabs下放置图标  编辑频道的图标 -->
-     <span class="bar_btn">
+     <!-- 注册点击事件 显示频道编辑 -->
+     <span class="bar_btn" @click="showChannelEdit=true">
         <!-- 放入图标 vant图标 -->
        <van-icon name="wap-nav"></van-icon>
      </span>
-     <!-- 反馈弹层组件 v-model控制弹层组件是否显示,默认为隐藏 -->
+      <!-- 反馈弹层组件 v-model控制弹层组件是否显示,默认为隐藏 -->
       <van-popup v-model="showMoreAction" style="width:80%">
         <!--不感兴趣\举报公用一个方法-->
         <!-- 监听more-action中的不感兴趣和举报的自定义事件,调用接口 -->
@@ -23,26 +24,35 @@
         <!-- report的第一个参数item.value 举报的类型 作为type的实参 -->
         <MoreAction @dislike="dislikeOrReport('dislike')" @report="dislikeOrReport('report',$event)" />
       </van-popup>
+        <!-- 频道编辑 -->
+        <!-- v-model 控制显示和隐藏 -->
+        <!-- title 标题 -->
+      <van-action-sheet :round="false" v-model="showChannelEdit" title="频道编辑">
+        <channelEdit></channelEdit>
+      </van-action-sheet>
   </div>
 </template>
 
 <script>
 import ArticleList from './components/article-list'
-import { getmyChannels } from '@/api/channels'
 import MoreAction from './components/more-action'
+import channelEdit from './components/channel-edit'
+import { getmyChannels } from '@/api/channels'
 import { dislikeArticle, reportArticle } from '@/api/articles' // 引入不感兴趣和举报的接口
 import eventbus from '@/utils/eventbus' // 公共事件池
+
 export default {
   name: 'home',
   components: {
-    ArticleList, MoreAction
+    ArticleList, MoreAction, channelEdit
   },
   data () {
     return {
       channels: [], // 接收频道数据
       showMoreAction: false, // 控制反馈组件显示隐藏
       articleId: null, // 接收article-list点击的文章id
-      activeIndex: 0 // 当前默认激活的页签
+      activeIndex: 0, // 当前默认激活的页签
+      showChannelEdit: false // 是否显示频道编辑组件
     }
   },
   methods: {
@@ -91,6 +101,18 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+// 处理频道编辑的样式
+.van-action-sheet {
+  max-height: 100%;
+  height: 100%;
+  .van-action-sheet__header {
+    background: #3296fa;
+    color: #fff;
+    .van-icon-close {
+      color: #fff;
+    }
+  }
+}
 .van-tabs {
   height: 100%;
   display: flex;
